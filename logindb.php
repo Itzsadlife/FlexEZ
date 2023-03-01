10f6a127439e5a6bae3b5e28e030138fa0cdb246
ItzSadlife <108647602+Itzsadlife@users.noreply.github.com>
2023-03-01 23:08:27 +0800

    Add files via upload

---
+++ logindb.php
@@ -0,0 +1,48 @@
+<?php 
+session_start(); 
+include "db.php";
+
+if (isset($_POST['uname']) && isset($_POST['password'])) {
+
+	function validate($data){
+       $data = trim($data);
+	   $data = stripslashes($data);
+	   $data = htmlspecialchars($data);
+	   return $data;
+	}
+
+	$uname = validate($_POST['uname']);
+	$pass = validate($_POST['password']);
+
+	if (empty($uname)) {
+		header("Location: login.php?error=User Name is required");
+	    exit();
+	}else if(empty($pass)){
+        header("Location: login.php?error=Password is required");
+	    exit();
+	}else{
+		$sql = "SELECT * FROM employee WHERE employeeID='$uname' AND password='$pass'";
+
+		$result = mysqli_query($db, $sql);
+
+		if (mysqli_num_rows($result) === 1) {
+			$row = mysqli_fetch_assoc($result);
+            if ($row['employeeID'] === $uname && $row['password'] === $pass) {
+            	$_SESSION['employeeID'] = $row['employeeID'];
+            	$_SESSION['name'] = $row['name'];
+            	header("Location: Submit.php");
+		        exit();
+            }else{
+				header("Location: login.php?error=Incorect User name or password");
+		        exit();
+			}
+		}else{
+			header("Location: login.php?error=Incorect User name or password");
+	        exit();
+		}
+	}
+	
+}else{
+	header("Location: login.php");
+	exit();
+}
