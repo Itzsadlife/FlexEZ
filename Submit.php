8f408721de5372885d3c98028024a9dce525b4c3
ItzSadlife <108647602+Itzsadlife@users.noreply.github.com>
2023-03-06 00:05:19 +0800

    Add files via upload

--- Submit.php
+++ Submit.php
@@ -6,20 +6,22 @@ session_start();
 
 if (isset($_SESSION['employeeID']) && isset($_SESSION['name'])) {
     ?>
- 
-<!DOCTYPE html>
-<html lang="en">
+
+    <!DOCTYPE html>
+    <html lang="en">
+
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>FlexIS | Register Employee </title>
+        <title>FlexEZ | Submit FWA Request </title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="KaiLiang.css">
         <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" 
-         integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
+            integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
     </head>
+
     <body>
         <div class="logoBanner">
             <img src="logo.jpg" alt="FlexIS">
@@ -32,7 +34,8 @@ if (isset($_SESSION['employeeID']) && isset($_SESSION['name'])) {
                     <nav>
                         <div class="row row-cols-5 row-cols-md-1">
                             <div class="col py-3 text-center text-md-left">Menu</div>
-                            <div class="col py-3 text-center text-md-left"><a href="register.html">Register Employee</a></div>
+                            <div class="col py-3 text-center text-md-left"><a href="register.html">Register Employee</a>
+                            </div>
                             <div class="col py-3 text-center text-md-left"><a href="">View FWA Analytics</a></div>
                             <div class="col py-3 text-center text-md-left"><a href="">Logout</a></div>
                         </div>
@@ -42,65 +45,73 @@ if (isset($_SESSION['employeeID']) && isset($_SESSION['name'])) {
                     <main>
                         <hr>
                         <div id="EmployeeForm">
-                            <h3>Register Employee</h3><hr>
-                            <form action="submitdb.php" method = "POST">
+                            <h3>Submit FWA Request</h3>
+                            <hr>
+                            <form action="submitdb.php" method="POST">
                                 <label> Employee ID </label>
-                                <input type="text" class="form-control col-sm-4 col-form-label" id="ID" value= "<?php echo $_SESSION['employeeID']; ?>" readonly>
+                                <input type="text" class="form-control col-sm-4 col-form-label" id="ID"
+                                    value="<?php echo $_SESSION['employeeID']; ?>" readonly>
                                 <label> Name </label>
-                                <input type="text" class="form-control col-sm-4 col-form-label" id="Name" value="<?php echo $_SESSION['name']; ?>" readonly>
+                                <input type="text" class="form-control col-sm-4 col-form-label" id="Name"
+                                    value="<?php echo $_SESSION['name']; ?>" readonly>
                                 <div class="form-group row">
                                     <label for="workType" class="col-sm-4 col-form-label">Work Type</label>
                                     <div class="col-sm-10">
-                                    <select name='workType' id='workType' class="form-control col-sm-5 col-form-label">
-                                        <option selected >(Select Work Type)</option>
-                                        <option value='Work From Home' id="workType1" name="workType1">Work From Home</option>
-                                        <option value='Flexi Hours' id="workType1" name="workType2">Flexi Hours</option>
-                                        <option value='Hybrid' id="workType1" name="workType3">Hybrid</option>
-                                    </select>
+                                        <select name='workType' id='workType' class="form-control col-sm-5 col-form-label">
+                                            <option value='Work From Home'>Work From Home</option>
+                                            <option value='Flexi Hours'>Flexi Hours</option>
+                                            <option value='Hybrid'>Hybrid</option>
+                                        </select>
                                     </div>
-                                  </div>
-                                  <div class="form-group row">
+                                </div>
+                                <div class="form-group row">
                                     <label for="description" class="col-sm-4 col-form-label">Description</label>
                                     <div class="col-sm-10">
-                                      <textarea type="text" class="form-control" id="description" name="description" placeholder="Description"></textarea>
+                                        <textarea type="text" class="form-control" id="description" name="description"
+                                            placeholder="Description"></textarea>
                                     </div>
-                                  </div>
-                                  <fieldset class="form-group">
+                                </div>
+                                <fieldset class="form-group">
                                     <div class="row">
                                         <label for="Reason" class="col-sm-4 col-form-label">Reason</label>
                                         <div class="col-sm-10">
-                                          <textarea type="text" class="form-control" id="Reason" name="Reason" placeholder="Reason"></textarea>
+                                            <textarea type="text" class="form-control" id="Reason" name="Reason"
+                                                placeholder="Reason"></textarea>
                                         </div>
                                     </div>
-                                  </fieldset>
-                                  <div class="form-group row">
+                                </fieldset>
+                                <div class="form-group row">
                                     <div class="col-sm-10">
-                                      <button type="submit" name="submit" class="btn btn-primary" data-toggle="modal" data-target="#verModal">Submit</button>
+                                        <button type="submit" name="submit" class="btn btn-primary">Submit</button>
                                     </div>
-                                  </div>
-                              </form>
+                                </div>
+                            </form>
                         </div>
-                    </main>
-                    <br>
                 </div>
-                <div class="col-md-1"></div>
             </div>
+            </main>
+            <br>
+        </div>
+        <div class="col-md-1"></div>
+        </div>
         </div>
         <div class="footer">
             <small><i>Copyright &copy; 2023 HELP University</i></small>
         </div>
 
-        <!-- JavaScript -->
-        <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" 
-        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" 
-        integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+        <!-- jQuery -->
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+        <!-- Bootstrap JS -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js"
+            integrity="sha384-AZ+T2QJdAJlb+NPEZhG8JvPc7b2rmVBbdEaq+J1YnHRmZ7LRPZo+poYGDBxFP8R/"
+            crossorigin="anonymous"></script>
     </body>
-</html>
-<?php
-}
-else{
-    header("Location: index.php");
+
+    </html>
+    <?php
+} else {
+    header("Location: login.php");
     exit();
 }
 ?>
--- submitdb.php
+++ submitdb.php
@@ -1,8 +1,7 @@
-<?php 
-    
-    include "db.php";
-session_start(); 
-$employeeID = $_SESSION['employeeID'];
+<?php
+
+include "db.php";
+session_start();
 
 // generate a random ID
 $randomID = 'R' . str_pad(mt_rand(1, 999), 3, '0', STR_PAD_LEFT);
@@ -12,30 +11,165 @@ $randomID = 'R' . str_pad(mt_rand(1, 999), 3, '0', STR_PAD_LEFT);
 $sql = "SELECT * FROM request WHERE requestID = '$randomID'";
 $checkingResult = mysqli_query($db, $sql);
 
-if(mysqli_num_rows($checkingResult) > 0) {
+if (mysqli_num_rows($checkingResult) > 0) {
     // the ID already exists in the database, regenerate the ID
     $randomID = 'R' . str_pad(mt_rand(1, 999), 3, '0', STR_PAD_LEFT);
 }
 
-if(isset($_POST['submit'])){
+if (isset($_POST['submit'])) {
 
-// Set the timezone to Malaysia
-date_default_timezone_set('Asia/Kuala_Lumpur');
+    // Set the timezone to Malaysia
+    date_default_timezone_set('Asia/Kuala_Lumpur');
 
-// Get the current date in the format of "DD-MM-YYYY"
-$localDate = date('d-m-Y');
+    // Get the current date in the format of "DD-MM-YYYY"
+    $localDate = date('d-m-Y');
 
-// Convert the local date format to "YYYY-MM-DD"
-$convertedDate = date('Y-m-d', strtotime($localDate));
+    // Convert the local date format to "YYYY-MM-DD"
+    $convertedDate = date('Y-m-d', strtotime($localDate));
 
 
-$workType= $_POST['workType']; // Retrieve the ID value from the form
-$Description = $_POST['description']; // Retrieve the name value from the form
-$Reason = $_POST['Reason']; // Retrieve the name value from the form
+    $workType = $_POST['workType']; // Retrieve the ID value from the form
+    $Description = $_POST['description']; // Retrieve the name value from the form
+    $Reason = $_POST['Reason']; // Retrieve the name value from the form
 
-$query = "INSERT INTO request (requestID, employeeID, requestDate, workType, description, reason, FWAstatus)
-            VALUES ('$randomID', '$employeeID', '$convertedDate', '$workType', '$Description', '$Reason', 'Pending')";
+    //checking if the employee has Supervisor or not
+    $sql = "SELECT * FROM employee WHERE SupervisorID IS NOT NULL AND employeeID = '$_SESSION[employeeID]'";
+    $result = mysqli_query($db, $sql);
+    $row = mysqli_num_rows($result);
 
-$result=mysqli_query($db,$query);
+    if (mysqli_num_rows($result) == 1) {
+        $query = "INSERT INTO request (requestID, employeeID, requestDate, workType, description, reason, FWAstatus)
+            VALUES ('$randomID', '$_SESSION[employeeID]', '$convertedDate', '$workType', '$Description', '$Reason', 'Pending')";
+
+        $result = mysqli_query($db, $query);
+        echo "
+        <!DOCTYPE html>
+        <html lang='en'>
+
+        <head>
+            <meta charset='utf-8'>
+            <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+            <title>FlexIS | Register Employee </title>
+            <meta name='description' content=''>
+            <meta name='viewport' content='width=device-width, initial-scale=1'>
+            <link rel='stylesheet' href='KaiLiang.css'>
+            <!-- Bootstrap CSS -->
+            <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css' integrity='sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N' crossorigin='anonymous'>
+        </head>
+
+        <body>
+            <div class='logoBanner'>
+                <img src='logo.jpg' alt='FlexIS'>
+            </div>
+            <div class='colorDiv'></div>
+            <div class='container-fluid'>
+                <div class='row'>
+                    <div class='col-md-1'></div>
+                    <div class='col-md-3 bg-secondary sticky-top'>
+                        <nav>
+                            <div class='row row-cols-5 row-cols-md-1'>
+                                <div class='col py-3 text-center text-md-left'>Menu</div>
+                                <div class='col py-3 text-center text-md-left'><a href='register.html'>Register Employee</a></div>
+                                <div class='col py-3 text-center text-md-left'><a href=''>View FWA Analytics</a></div>
+                                <div class='col py-3 text-center text-md-left'><a href=''>Logout</a></div>
+                            </div>
+                        </nav>
+                    </div>
+                    <div class='col-md-7 bg-light'>
+                        <main>
+                            <hr>
+                            <div class='alert alert-success' role='alert'>
+                                <h4 class='alert-heading'>Congratulation!</h4>
+                                <p>You have successful submitted a FWA request.</p>
+                                <hr>
+                                <p class='mb-0'>Your request ID is $randomID</p>
+                            </div>
+
+                    </div>
+                </div>
+                </main>
+                <br>
+            </div>
+            <div class='col-md-1'></div>
+            </div>
+            </div>
+            <div class='footer'>
+                <small><i>Copyright &copy; 2023 HELP University</i></small>
+            </div>
+
+            <!-- jQuery -->
+            <script src='https://code.jquery.com/jquery-3.6.0.min.js'></script>
+
+            <!-- Bootstrap JS -->
+            <script src='https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js' integrity='sha384-AZ+T2QJdAJlb+NPEZhG8JvPc7b2rmVBbdEaq+J1YnHRmZ7LRPZo+poYGDBxFP8R/' crossorigin='anonymous'></script>
+        </body>
+
+        </html>";
+
+    } else {
+        echo "
+        <!DOCTYPE html>
+        <html lang='en'>
+
+        <head>
+            <meta charset='utf-8'>
+            <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+            <title>FlexIS | Register Employee </title>
+            <meta name='description' content=''>
+            <meta name='viewport' content='width=device-width, initial-scale=1'>
+            <link rel='stylesheet' href='KaiLiang.css'>
+            <!-- Bootstrap CSS -->
+            <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css' integrity='sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N' crossorigin='anonymous'>
+        </head>
+
+        <body>
+            <div class='logoBanner'>
+                <img src='logo.jpg' alt='FlexIS'>
+            </div>
+            <div class='colorDiv'></div>
+            <div class='container-fluid'>
+                <div class='row'>
+                    <div class='col-md-1'></div>
+                    <div class='col-md-3 bg-secondary sticky-top'>
+                        <nav>
+                            <div class='row row-cols-5 row-cols-md-1'>
+                                <div class='col py-3 text-center text-md-left'>Menu</div>
+                                <div class='col py-3 text-center text-md-left'><a href='register.html'>Register Employee</a></div>
+                                <div class='col py-3 text-center text-md-left'><a href=''>View FWA Analytics</a></div>
+                                <div class='col py-3 text-center text-md-left'><a href=''>Logout</a></div>
+                            </div>
+                        </nav>
+                    </div>
+                    <div class='col-md-7 bg-light'>
+                        <main>
+                            <hr>
+                            <div class='alert alert-danger' role='alert'>
+                                <h4 class='alert-heading'>Error!</h4>
+                                <p>You cannot submit a FWA request without a supervisor.</p>
+                                <hr>
+                                <p class='mb-0'>Please contact HR if you have any questions.</p>
+                            </div>
+
+                    </div>
+                </div>
+                </main>
+                <br>
+            </div>
+            <div class='col-md-1'></div>
+            </div>
+            </div>
+            <div class='footer'>
+                <small><i>Copyright &copy; 2023 HELP University</i></small>
+            </div>
+
+            <!-- jQuery -->
+            <script src='https://code.jquery.com/jquery-3.6.0.min.js'></script>
+
+            <!-- Bootstrap JS -->
+            <script src='https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js' integrity='sha384-AZ+T2QJdAJlb+NPEZhG8JvPc7b2rmVBbdEaq+J1YnHRmZ7LRPZo+poYGDBxFP8R/' crossorigin='anonymous'></script>
+        </body>
+
+        </html>";
+    }
 }
 ?>
