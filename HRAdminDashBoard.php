328ac6aa4c5d31de852f11fd4e5b852239afe8bd
porsie <86921293+porsie@users.noreply.github.com>
2023-03-06 11:33:19 +0800

    Update HRAdminDashBoard.php

--- HRAdminDashBoard.php
+++ HRAdminDashBoard.php
@@ -11,8 +11,8 @@ if (isset($_SESSION['employeeID']) && isset($_SESSION['name'])) {
     <h3>Welcome to the HR Admin Dashboard</h3>
     <label for="name"> <?php echo "Employee Name : $_SESSION[name] ";?> </label><br><br><br>
     <label for="empID"><?php echo "Employee ID : $_SESSION[employeeID] ";?> </label><br><br><br>
-    <label for="position">Position: IT developer</label><br><br><br>
-    <label for="email">Email: iamcubex@gmail.com </label><br><br><br>
+    <label for="position">Position: HR Admin</label><br><br><br>
+    <label for="email">Email: HRAdmin@gmail.com </label><br><br><br>
 
     <button type="button" class="btn btn-success">Edit Profile</button>
 
