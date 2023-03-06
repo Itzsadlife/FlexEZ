6b30947e5237438ac58b8847a81ae987b807b0ba
porsie <86921293+porsie@users.noreply.github.com>
2023-03-06 10:48:53 +0800

    dashboard

---
+++ HRAdminDashboard.php
@@ -0,0 +1,17 @@
+<?php include 'header.php';?>
+
+
+<main>
+    <hr>
+    <h3>Welcome to the HR Admin Dashboard</h3>
+    <label for="name">Name: </label><br><br><br>
+    <label for="empID">Employee ID: </label><br><br><br>
+    <label for="position">Position: HR Admin</label><br><br><br>
+    <label for="email">Email: </label><br><br><br>
+
+    <button type="button" class="btn btn-success">Edit Profile</button>
+
+        
+</main>
+<br>
+<?php include 'footer.php'; ?>
