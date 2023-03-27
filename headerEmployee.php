466a6d464575993e5e567c94ffdd4f90f37ac428
ItzSadlife <108647602+Itzsadlife@users.noreply.github.com>
2023-03-27 10:44:08 +0800

    updated headers for navigation

---
+++ headerEmployee.php
@@ -0,0 +1,42 @@
+<?php include 'db.php'; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>FlexEZ | Register Employee </title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
+        <meta http-equiv="Pragma" content="no-cache">
+        <meta http-equiv="Expires" content="0">
+        <link rel="stylesheet" href="PorSie.css">
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" 
+         integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        
+
+    </head>
+    <body>
+        <div class="logoBanner">
+            <img src="img/logo.jpg" alt="FlexEZ">
+        </div>
+        <div class="colorDiv"></div>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-1"></div>
+                <div class="col-md-3 bg-secondary sticky-top">
+                    <nav>
+                        <div class="row row-cols-5 row-cols-md-1">
+                            <div class="col py-3 text-center text-md-left">Menu</div>
+                            <div class="col py-3 text-center text-md-left"><a href="Submit.php">Submit FWA Request</a></div>
+                            <div class="col py-3 text-center text-md-left"><a href="">Update Daily Schedule</a></div>
+                            <div class="col py-3 text-center text-md-left"><a href="logout.php">Logout</a></div>
+                        </div>
+                    </nav>
+                </div>
+                <div class="col-md-7 bg-light">
